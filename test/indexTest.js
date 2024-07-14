1eb80778f704e326c3efce7217f14d87e303bb6b
Cindy <cindyakumu40@gmail.com>
2024-07-14 22:03:36 +0300

    completed lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,25 @@
 // The code below ensures that students who are using CodeGrade will get credit
 // for the code-along in Canvas; you can disregard it.
+{
+  "articles";[
+    {
+      "id": 1,
+      "title": "Example Article",
+      "content": "This is an example."
+    },
+    {
+      "id": 2,
+      "title": "Second Article",
+      "content": "This is also an example."
+    },
+    {
+      "id": 3,
+      "title": "Third Article",
+      "content": "Another example!"
+    }
+  ]
+}
+
 
 require("./helpers.js");
 
